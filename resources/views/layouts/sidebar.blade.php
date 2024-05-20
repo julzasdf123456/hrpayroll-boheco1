7a58b213103ccf13b3f1c183e473a5a5278bb472
Julio Lopez <julzasdf123456@gmail.com>
2024-05-20 16:24:39 +0800

    updates

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -31,7 +31,7 @@
                         'overtimes.create',
                         'attendanceConfirmations.create',
                         'leaveConversions.create',
-                        'home.chat', 'home.chat-reeve',
+                        'home.chat', 'home.chat-reeve', 'home.reeve',
                         'leaveApplications.my-approvals', 'tripTickets.my-approvals', 'offsetApplications.my-approvals', 'attendanceConfirmations.my-approvals', 'overtimes.my-approvals',
                         'travelOrders.create', 'travelOrders.my-approvals',
                         ]))
