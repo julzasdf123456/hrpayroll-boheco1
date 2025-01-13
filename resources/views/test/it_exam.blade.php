005187068569273b2b642a833c0ce57a16a0e85a
Julio Lopez <julzasdf123456@gmail.com>
2025-01-13 13:27:10 +0800

    ocal

--- resources/views/test/it_exam.blade.php
+++ resources/views/test/it_exam.blade.php
@@ -1,73 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>IT Exam</title>
+@extends('layouts.app')
 
-    <link rel="stylesheet" href="{{ URL::asset('css/style.css'); }}">
-
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
-          integrity="sha512-0S+nbAYis87iX26mmj/+fWt1MmaKCv80H+Mbo+Ne7ES4I6rxswpfnC6PxmLiw33Ywj2ghbtTw0FkLbMWqh4F7Q=="
-          crossorigin="anonymous"/>
-
-    <!-- AdminLTE -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.0.5/css/adminlte.min.css"
-          integrity="sha512-rVZC4rf0Piwtw/LsgwXxKXzWq3L0P6atiQKBNuXYRbg2FoRbSTIY0k2DxuJcs7dk4e/ShtMzglHKBOJxW8EQyQ=="
-          crossorigin="anonymous"/>
-
-    <!-- iCheck -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/icheck-bootstrap/3.0.1/icheck-bootstrap.min.css"
-          integrity="sha512-8vq2g5nHE062j3xor4XxPeZiPjmRDh6wlufQlfC6pdQ/9urJkU07NM0tEREeymP++NczacJ/Q59ul+/K2eYvcg=="
-          crossorigin="anonymous"/>
-
-    <link rel="stylesheet" href="{{ URL::asset('css/source_sans_pro.css'); }} ">
-
-    <link rel="stylesheet" href="{{ URL::asset('css/all.css'); }} ">
-
-    <link rel="stylesheet" href="{{ URL::asset('css/bootstrap4toggle.css'); }} ">
-          
-    <link rel="stylesheet" href="{{ URL::asset('css/adminlte.min.css'); }} ">
-
-    <link rel="stylesheet" href="{{ URL::asset('css/bootstrapdatetimepicker.min.css'); }} ">
-
-    <link rel="stylesheet" href="{{ URL::asset('css/jqueryui.css'); }} ">
-
-    <link rel="stylesheet" href="{{ URL::asset('css/select2.min.css'); }} ">
-
-    <link rel="stylesheet" href="{{ URL::asset('css/select2.bootstrap4.min.css'); }} ">
-
-    <link rel="stylesheet" href="{{ URL::asset('css/fullcalendar.min.css'); }} ">
-
-    <link rel="stylesheet" href="{{ URL::asset('css/sweetalert2.min.css'); }}">
-
-    <link rel="stylesheet" href="{{ URL::asset('css/daterangepicker.min.css'); }}">
-
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-<body class="content">
-
-    {{-- OTHERS --}}
-    <div id="app">
-        <it-exam></it-exam>
-    </div>
-    @vite('resources/js/app.js')
-
-<!-- AdminLTE App -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.0.5/js/adminlte.min.js"
-        integrity="sha512-++c7zGcm18AhH83pOIETVReg0dr1Yn8XTRw+0bWSIWAVCAwz1s2PwnSj4z/OOyKlwSXc4RLg3nnjR22q0dhEyA=="
-        crossorigin="anonymous"></script>
-
-</body>
-</html>
+@section('content')
+{{-- OTHERS --}}
+<div id="app">
+    <it-exam></it-exam>
+</div>
+@vite('resources/js/app.js')
+@endsection
